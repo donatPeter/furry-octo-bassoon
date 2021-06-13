6b868b058a0964f4d32e75cac40423997e661a50
donatPeter <peterdonat.bme@gmail.com>
2021-06-13 15:36:30 +0200

    refactor useRepos hook and use it

--- src/hooks/useRepos.ts
+++ src/hooks/useRepos.ts
@@ -1,13 +1,22 @@
-import { useQuery } from "react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Repo } from "../types/repo.type";
 
 export const useRepos = () => {
-  const [repos, setRepos] = useState<Repo[]>();
-  const { isLoading, error } = useQuery("repoList", () =>
-    fetch("https://api.github.com/orgs/github/repos").then(async (res) => {
-      setRepos(await res.json());
-    })
-  );
+  const [repos, setRepos] = useState<Repo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError(false);
+
+    fetch("https://api.github.com/orgs/github/repos")
+      .then(async (res) => {
+        setRepos(await res.json());
+        setIsLoading(false);
+      })
+      .catch(() => setError(true));
+  }, []);
+
   return { isLoading, error, repos };
 };
--- src/pages/RepoList.tsx
+++ src/pages/RepoList.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { useRepos } from "../hooks/useRepos";
 import { ProgressBar } from "../components/ProgressBar";
+import { Error } from "./Error";
 
 export const RepoList = () => {
-  const { isLoading } = useRepos();
+  const { isLoading, error, repos } = useRepos();
 
-  return <>{isLoading && <ProgressBar />}</>;
+  return (
+    <>
+      {isLoading && <ProgressBar />}
+      {!isLoading && error && <Error />}
+      {!isLoading && repos.map((repo) => <p key={repo.name}>{repo.name}</p>)}
+    </>
+  );
 };
