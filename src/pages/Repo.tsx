78b8b3766e063982c8907875f9034aa61797bf39
donatPeter <peterdonat.bme@gmail.com>
2021-06-13 16:44:54 +0200

    add repo details page

--- src/pages/Repo.tsx
+++ src/pages/Repo.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { ProgressBar } from "../components/ProgressBar";
 import { Error } from "./Error";
 import { useRepos } from "../hooks/useRepos";
-import { Box } from "@material-ui/core";
+import { Box, Typography } from "@material-ui/core";
 
 export const Repo = () => {
   const { id } = useParams<{ id: string }>();
@@ -14,7 +14,30 @@ export const Repo = () => {
     <>
       {isLoading && <ProgressBar />}
       {!isLoading && error && <Error />}
-      {!isLoading && !error && repo && <Box>{repo.name}</Box>}
+      {!isLoading && !error && repo && (
+        <Box padding="2rem" display="flex" flexDirection="column">
+          <Box
+            width="100%"
+            display="flex"
+            justifyContent="center"
+            marginBottom="4rem"
+          >
+            <Typography variant="h1">{repo.name}</Typography>
+          </Box>
+          <Box>
+            <Box display="flex">
+              <p>name: </p>
+              <p>{repo.name}</p>
+            </Box>
+          </Box>
+          <Box>
+            <Box display="flex">
+              <p>description: </p>
+              <p>{repo.description}</p>
+            </Box>
+          </Box>
+        </Box>
+      )}
     </>
   );
 };
